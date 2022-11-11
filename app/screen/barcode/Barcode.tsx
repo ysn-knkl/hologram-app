f26889c3c2dd6fcf6f5b84f9417bf00b7041a543
Yasin okunakol <yasinokunakol@gmail.com>
2022-11-11 22:46:20 +0300

    camera

--- app/screen/barcode/Barcode.tsx
+++ app/screen/barcode/Barcode.tsx
@@ -1,5 +1,4 @@
 import {
-  Linking,
   PermissionsAndroid,
   Platform,
   SafeAreaView,
@@ -8,146 +7,158 @@ import {
   View,
 } from "react-native";
 import React, { useState } from "react";
-import { Text, Layout, Input, Button } from "@ui-kitten/components";
+import {
+  Text,
+  Layout,
+  Input,
+  Button,
+  Modal,
+  Card,
+} from "@ui-kitten/components";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { addBarcode } from "../../redux/features/barcodeSlice";
-import { Camera, CameraType } from "react-native-camera-kit";
-import CameraKitCameraScreen from 'react-native-camera-kit';
+import { Camera, CameraType, CameraScreen } from "react-native-camera-kit";
+import CustomAlert from "../../components/CustomAlert";
 
 type Props = {};
 
 const Barcode = (props: Props) => {
-  const [value, setValue] = useState("");
   const [qrvalue, setQrvalue] = useState("");
   const [opneScanner, setOpneScanner] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [visibleAlert, setVisibleAlert] = useState("");
 
   const dispatch = useAppDispatch();
-  const { barcodeList } = useAppSelector((state) => state.barcode);
 
-  const onOpenlink = () => {
-    // If scanned then function to open URL in Browser
-    Linking.openURL(qrvalue);
-  };
+  const { barcodeList } = useAppSelector((state) => state.barcode);
 
   const onBarcodeScan = (qrvalue: React.SetStateAction<string>) => {
     // Called after te successful scanning of QRCode/Barcode
     setQrvalue(qrvalue);
+    handleSend();
     setOpneScanner(false);
   };
 
   const handleSend = () => {
-    dispatch(addBarcode(Number(value)));
-    setValue("");
+    dispatch(addBarcode(Number(qrvalue)));
+    setQrvalue("");
   };
 
-  const onOpneScanner = () => {
+  async function requestCameraPermission() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        // If CAMERA Permission is granted
+        setQrvalue("");
+        setOpneScanner(true);
+      } else {
+        console.log("CAMERA permission denied");
+      }
+    } catch (err) {
+      console.log("Camera permission err", err);
+      console.warn(err);
+    }
+  }
+
+  async function requestCameraPermissionIos() {
+    try {
+      const isCameraAuthorized =
+        await Camera.checkDeviceCameraAuthorizationStatus();
+      const isUserAuthorizedCamera =
+        await Camera.requestDeviceCameraAuthorization();
+
+      if (isCameraAuthorized === true && isUserAuthorizedCamera === true) {
+        setQrvalue("");
+        setOpneScanner(true);
+      } else {
+        setVisibleAlert("CAMERA permission denied");
+      }
+    } catch (error) {
+      setVisibleAlert("CAMERA permission denied");
+    }
+  }
+
+  const onOpneScanner = async () => {
+    setVisible(true);
     // To Start Scanning
     if (Platform.OS === "android") {
-      async function requestCameraPermission() {
-        try {
-          const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.CAMERA
-          );
-          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            // If CAMERA Permission is granted
-            setQrvalue("");
-            setOpneScanner(true);
-          } else {
-            console.log("CAMERA permission denied");
-          }
-        } catch (err) {
-          console.log("Camera permission err", err);
-          console.warn(err);
-        }
-      }
       // Calling the camera permission function
       requestCameraPermission();
     } else if (Platform.OS === "ios") {
-      console.log(1111);
-      async function requestCameraPermissionIos() {
-        try {
-          const isCameraAuthorized =
-            await Camera.checkDeviceCameraAuthorizationStatus();
-          const isUserAuthorizedCamera =
-            await Camera.requestDeviceCameraAuthorization();
-
-          if (isCameraAuthorized === true && isUserAuthorizedCamera === true) {
-            setQrvalue("");
-            setOpneScanner(true);
-          } else {
-            console.log("CAMERA permission denied11");
-          }
-        } catch (error) {
-          console.log("CAMERA permission denied11");
-        }
-      }
       requestCameraPermissionIos();
     }
   };
 
   return (
     <SafeAreaView style={styles.safeAreaContainer}>
-      <Layout style={styles.container}>
-        <View style={styles.inputContainer}>
-          <Input
-            style={styles.input}
-            value={value}
-            placeholder="Barcode"
-            keyboardType="numeric"
-            status="primary"
-            onChangeText={(nextValue) => setValue(nextValue)}
-          />
-          <Button onPress={handleSend}>Send</Button>
-        </View>
-
-        {barcodeList &&
-          barcodeList.map((item, i) => {
-            return <Text key={i}>{item}</Text>;
-          })}
-      </Layout>
-      {opneScanner ? (
-        <View style={{ flex: 1 }}>
-          <CameraKitCameraScreen
-            showFrame={false}
-            // Show/hide scan frame
-            scanBarcode={true}
-            // Can restrict for the QR Code only
-            laserColor={"blue"}
-            // Color can be of your choice
-            frameColor={"yellow"}
-            // If frame is visible then frame color
-            colorForScannerFrame={"black"}
-            // Scanner Frame color
-            onReadCode={(event: { nativeEvent: { codeStringValue: React.SetStateAction<string>; }; }) =>
-              onBarcodeScan(event.nativeEvent.codeStringValue)
-            }
-          />
-        </View>
-      ) : (
-        <View style={styles.container}>
-          <Text style={styles.titleText}>
-            Barcode and QR Code Scanner using Camera in React Native
-          </Text>
-          <Text style={styles.textStyle}>
-            {qrvalue ? "Scanned Result: " + qrvalue : ""}
-          </Text>
-          {qrvalue.includes("https://") ||
-          qrvalue.includes("http://") ||
-          qrvalue.includes("geo:") ? (
-            <TouchableHighlight onPress={onOpenlink}>
-              <Text style={styles.textLinkStyle}>
-                {qrvalue.includes("geo:") ? "Open in Map" : "Open Link"}
-              </Text>
+      <Layout style={styles.layoutContainer}>
+        <CustomAlert
+          visibleAlert={visibleAlert}
+          setVisibleAlert={setVisibleAlert}
+        />
+        <Button
+          onPress={() => {
+            setVisibleAlert("asd");
+          }}
+        >
+          deneme
+        </Button>
+        {opneScanner ? (
+          <>
+            <Camera
+              style={{ width: 300, height: 300 }}
+              cameraType={CameraType.Back}
+              scanBarcode={true}
+              onReadCode={(event: {
+                nativeEvent: {
+                  codeStringValue: React.SetStateAction<string>;
+                };
+              }) => onBarcodeScan(event.nativeEvent.codeStringValue)}
+              showFrame={true} // (default false) optional, show frame with transparent layer (qr code or barcode will be read on this area ONLY), start animation for scanner,that stoped when find any code. Frame always at center of the screen
+              laserColor={"blue"} // (default red) optional, color of laser in scanner frame
+              frameColor={"red"} // (default white) optional, color of border of scanner frame
+              focusMode={"false"}
+              ratioOverlay={"1:1"}
+              cameraRatioOverlay={undefined}
+              captureButtonImage={undefined}
+              captureButtonImageStyle={{}}
+              cameraFlipImage={undefined}
+              cameraFlipImageStyle={{}}
+              hideControls={undefined}
+              torchOnImage={undefined}
+              torchOffImage={undefined}
+              torchImageStyle={{}}
+              onBottomButtonPressed={function (event: any): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+            <Button
+              onPress={() => {
+                setOpneScanner(false);
+                setVisible(false);
+              }}
+            >
+              DISMISS
+            </Button>
+          </>
+        ) : (
+          <View style={styles.container}>
+            <Text style={styles.titleText}>
+              Barcode and QR Code Scanner using Camera in React Native
+            </Text>
+            <TouchableHighlight
+              onPress={onOpneScanner}
+              style={styles.buttonStyle}
+            >
+              <Text style={styles.buttonTextStyle}>Open Barcode Scanner</Text>
             </TouchableHighlight>
-          ) : null}
-          <TouchableHighlight
-            onPress={onOpneScanner}
-            style={styles.buttonStyle}
-          >
-            <Text style={styles.buttonTextStyle}>Open QR Scanner</Text>
-          </TouchableHighlight>
-        </View>
-      )}
+          </View>
+        )}
+
+        {<Text>Added Barcode</Text>}
+      </Layout>
     </SafeAreaView>
   );
 };
@@ -158,7 +169,9 @@ const styles = StyleSheet.create({
   safeAreaContainer: {
     flex: 1,
   },
-
+  layoutContainer: {
+    flex: 1,
+  },
   inputContainer: {
     flex: 1,
     width: "90%",
@@ -201,5 +214,8 @@ const styles = StyleSheet.create({
   textLinkStyle: {
     color: "blue",
     paddingVertical: 20,
+  },
+  backdrop: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
 });
